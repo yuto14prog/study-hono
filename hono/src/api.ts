9e113f954ceeed212d18ca62605313e5da81147f
yuto14prog <yuto14prog@gmailcom>
2024-08-31 20:19:04 +0900

    api追加

--- hono/src/api.ts
+++ hono/src/api.ts
@@ -1,9 +1,17 @@
+import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
+import { Users } from "../types/type";
 
 const api = new Hono()
+const prisma = new PrismaClient()
 
 api.get('/hello', (c) => {
     return c.json({ hello: 'hello api'})
+})
+
+api.get('/users', async (c) => {
+    const users: Users = await prisma.user.findMany()
+    return c.json(users)
 })
 
 export default api
---
+++ hono/types/type.ts
@@ -0,0 +1,7 @@
+export type User = {
+    id: number,
+    email: string,
+    name: string,
+}
+
+export type Users = User[]
