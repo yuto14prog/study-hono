27b87fbbd6b989aa61eaba832f79bb70f1a7602d
yuto14prog <yuto14prog@gmailcom>
2024-08-31 20:47:16 +0900

    getapi追加

--- hono/src/api.ts
+++ hono/src/api.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
-import { Users } from "../types/type";
+import { User, Users } from "../types/type";
 
 const api = new Hono()
 const prisma = new PrismaClient()
@@ -9,8 +9,26 @@ api.get('/hello', (c) => {
     return c.json({ hello: 'hello api'})
 })
 
+api.get('/users/:id', async (c) => {
+    const userId = Number(c.req.param('id'))
+    const user: User | null = await prisma.user.findUnique({
+        where: { id: userId }
+    })
+
+    if (user === null) {
+        return c.json({ error: 'user not found' }, 404)
+    }
+
+    return c.json(user)
+})
+
 api.get('/users', async (c) => {
-    const users: Users = await prisma.user.findMany()
+    const users: Users | null = await prisma.user.findMany()
+
+    if (users === null) {
+        return c.json({ error: 'users not found' }, 404)
+    }
+
     return c.json(users)
 })
 
